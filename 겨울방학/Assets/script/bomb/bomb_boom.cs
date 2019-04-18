99a4c1f72d6cce9a40102d0d2c53ed1ddaadd20e
Dead-DogBird <48205134+Dead-DogBird@users.noreply.github.com>
2019-04-18 22:22:09 +0900

    20190418

--- 겨울방학/Assets/script/bomb/bomb_boom.cs
+++ 겨울방학/Assets/script/bomb/bomb_boom.cs
@@ -15,23 +15,21 @@ public class bomb_boom : MonoBehaviour
     float a = 0.025f;
     bool isGround = false;
     public float firstnum, plus;
-<<<<<<< HEAD
-    public GameObject effect,outlineeffect;
-=======
-    public GameObject effect,effect_shadow;
->>>>>>> 85f03661486dce86e5d5bf3e0fe6423e2ddf3bfe
+    public GameObject effect, effect_shadow;
     float nextfireQ, firerateQ = 0.01f;
     GameObject inst;
     float Y, X;
     Vector3 moveboom;
     public GameObject pa;
     public bool kill;
+    
+    Transform shadow_transform;
     void Start()
     {
         a = firstnum;
         inst = Instantiate(shadow);
         inst.transform.position = transform.position + new Vector3(0, -0.35f, 0);
-        Y = transform.position.y - 0.75f;
+        Y = transform.position.y - 0.5f;
         X = transform.position.x;
         if (dir == (int)shoot_direction.left)
             moveboom = new Vector3(speed, 0, 0);
@@ -42,111 +40,94 @@ public class bomb_boom : MonoBehaviour
         if (dir == (int)shoot_direction.down)
             moveboom = new Vector3(0, -speed, 0);
         Instantiate(pa).transform.position = transform.position;
-       
-       
+        
+
     }
     bool Is_hit()
     {
         BoxCollider2D box_collider = transform.GetComponent<BoxCollider2D>();//컴포넌트를 얻어옴
         box_collider.enabled = false;//자신 충돌 체크 꺼줌
         Collider2D[] hit = Physics2D.OverlapBoxAll(transform.position + new Vector3(box_collider.offset.x, box_collider.offset.y),
-         new Vector2(box_collider.size.x * transform.localScale.x, box_collider.size.y * transform.localScale.y), 0,1<<8);//충돌  정보를 얻어옴
+         new Vector2(box_collider.size.x * transform.localScale.x, box_collider.size.y * transform.localScale.y), 0, 1 << 8);//충돌  정보를 얻어옴
         if (hit != null)
         {
             foreach (Collider2D i in hit)
             {
-                if(i.tag=="monster")
-                kill=true;
-            }         
+                if (i.tag == "monster")
+                    kill = true;
+            }
         }
         box_collider.enabled = true;//다시 자신의 충돌 체크를 켜줌			
         return (hit != null);//부딫혔을때 true반환 아닐경우에는 false 반환
     }
     void Die()
     {
-        
-            GameObject temp = Instantiate(effect);
-<<<<<<< HEAD
-            GameObject temp_sh = Instantiate(outlineeffect);
-=======
-            GameObject temp_sh = Instantiate(effect_shadow);
->>>>>>> 85f03661486dce86e5d5bf3e0fe6423e2ddf3bfe
-            temp.transform.position = transform.position + new Vector3(0, 0, -0.1f);
-            temp_sh.transform.position = transform.position + new Vector3(0, 0, -0.05f);
-            temp_sh.transform.rotation = temp.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
-            temp_sh.transform.localScale += new Vector3(0.2f, 0.2f, 0);
-            temp_sh.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
-            Destroy(inst);
-            Destroy(gameObject);
+
+        GameObject temp = Instantiate(effect);
+        GameObject temp_sh = Instantiate(effect_shadow);
+        temp.transform.position = transform.position + new Vector3(0, 0, -0.1f);
+        temp_sh.transform.position = transform.position + new Vector3(0, 0, -0.05f);
+        temp_sh.transform.rotation = temp.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+        temp_sh.transform.localScale += new Vector3(0.2f, 0.2f, 0);
+        temp_sh.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+        Destroy(inst);
+        Destroy(gameObject);
     }
     void CoolTime()
     {
-          GameObject tempOb = Instantiate(orbit);
-            GameObject tempObshadow = Instantiate(orbit);
+        GameObject tempOb = Instantiate(orbit);
+        GameObject tempObshadow = Instantiate(orbit);
 
-            tempOb.transform.position = transform.position + new Vector3(Random.Range(-0.07f, 0.07f), Random.Range(-0.07f, 0.07f));//랜덤하게 연기 퍼짐
+        tempOb.transform.position = transform.position + new Vector3(Random.Range(-0.07f, 0.07f), Random.Range(-0.07f, 0.07f));//랜덤하게 연기 퍼짐
 
-            float radonscle = Random.Range(-0.11f, 0.11f);
-            tempOb.transform.localScale += new Vector3(radonscle, radonscle);
-            
-            tempObshadow.transform.position = tempOb.transform.position + new Vector3(0, 0, 0.1f);
-            tempObshadow.transform.localScale = tempOb.transform.localScale + new Vector3(0.17f, 0.17f, 0);
-            tempObshadow.gameObject.GetComponent<Renderer>().material.color = new Color(0.2f, 0.2f, 0.2f);
-            nextfireQ = Time.time + firerateQ;
+        float radonscle = Random.Range(-0.11f, 0.11f);
+        tempOb.transform.localScale += new Vector3(radonscle, radonscle);
+
+        tempObshadow.transform.position = tempOb.transform.position + new Vector3(0, 0, 0.1f);
+        tempObshadow.transform.localScale = tempOb.transform.localScale + new Vector3(0.17f, 0.17f, 0);
+        tempObshadow.gameObject.GetComponent<Renderer>().material.color = new Color(0.2f, 0.2f, 0.2f);
+        nextfireQ = Time.time + firerateQ;
     }
+    float g = 0.15f;
+    float ga = 0.005f;
+
+    float colorr=4.5f;
     // Update is called once per frame
     void Update()
-    {   
+    {
         Is_hit();
         if (Time.time > nextfireQ)
         {
-          CoolTime();
+            CoolTime();
         }
         transform.position += moveboom;
         if (speed >= 0)
             speed -= 0.0025f;
         if (speed < 0)
             speed = 0;
-        a -= plus;
-        i_z += a;
-        if (dir == (int)shoot_direction.right || dir == (int)shoot_direction.left)
-        {
-            if (i_z <= -0.4f && isGround == false)
-            {
-                a = firstnum / 1.2f;
-                i_z = 0f;
-                isGround = true;
-            }
-            if (i_z > -0.2f || isGround == false)
-            {
-                transform.position += new Vector3(0, i_z, 0);
-                inst.transform.localScale -= new Vector3(i_z / 4, i_z / 4);
-            }
-            inst.transform.position = new Vector3(transform.position.x, Y, transform.position.z);
-        }
-        if (dir == (int)shoot_direction.up || dir == (int)shoot_direction.down)
-        {
-            if (i_z <= -0.4f && isGround == false)
-            {
-                a = firstnum / 1.2f;
-                i_z = 0f;
-                isGround = true;
-            }
-            if (i_z > -0.2f || isGround == false)
-            {
-                transform.localScale += new Vector3(i_z / 4, i_z / 4, 0);
-            }
-            inst.transform.position = new Vector3(X, transform.position.y - 0.45f + (-i_z * 2), transform.position.z);
-        }
+        
         transform.localScale += new Vector3(0.005f, 0.005f, 0);
+
+        if (dir == (int)shoot_direction.left || dir == (int)shoot_direction.right)
+        {
+            transform.position += new Vector3(0, g, 0);
+            g -= ga;
+            ga += 0.0005f;
+            if (transform.position.y <= Y)
+            {
+                ga = 0.005f;
+                g = 0.075f;
+            }
+            inst.transform.position=new Vector2(transform.position.x,Y-0.5f);
+        }
         if (dir != (int)shoot_direction.left)
-            transform.Rotate(Vector3.forward * Time.deltaTime * 5000 * speed);
+            transform.Rotate(Vector3.forward * Time.deltaTime * 75000 * speed);
         else
         {
-            transform.Rotate(Vector3.back * Time.deltaTime * 5000 * speed);
+            transform.Rotate(Vector3.back * Time.deltaTime * 75000 * speed);
         }
 
-        if (speed <= 0||kill==true)
+        if (speed <= 0 || kill == true)
         {
             Die();
         }
