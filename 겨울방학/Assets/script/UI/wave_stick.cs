8301e2f2b466607ab127ffd1f169e9fdccea95c9
Dead-DogBird <48205134+Dead-DogBird@users.noreply.github.com>
2019-05-21 16:27:58 +0900

    20190521

--- 겨울방학/Assets/script/UI/wave_stick.cs
+++ 겨울방학/Assets/script/UI/wave_stick.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class wave_stick : MonoBehaviour
 {
     float x;
+    Image myImage;
     // Start is called before the first frame update
     void Start()
     {
       x=transform.localScale.x;
+        myImage = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
        if(monster_manager.Instance.player!=null)
-        transform.localScale=new Vector3(((monster_manager.Instance.nextfireQ-Time.time)/monster_manager.Instance.firerateQ),1);
+       myImage.fillAmount= ((monster_manager.Instance.nextfireQ - Time.time) / monster_manager.Instance.firerateQ);
+       // transform.localScale=new Vector3((,1);
     }
 }
