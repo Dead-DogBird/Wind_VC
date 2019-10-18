abac4e014333cc594c974cb63ddf021900ddf36c
Dead-DogBird <48205134+Dead-DogBird@users.noreply.github.com>
2019-10-19 00:29:55 +0900

    20191019

--- wind_vac/Assets/script/UI/mainmenu/AcadeUnLock.cs
+++ wind_vac/Assets/script/UI/mainmenu/AcadeUnLock.cs
@@ -6,10 +6,16 @@ public class AcadeUnLock : MonoBehaviour
 {
     public Text acadeName,acadeInfo;
     public int acadeCode;
+    public string OriName,OriInfo;
+    public string path;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(PlayerPrefs.HasKey(path))
+        {
+            acadeName.text=OriName;
+
+        }
     }
 
     // Update is called once per frame
